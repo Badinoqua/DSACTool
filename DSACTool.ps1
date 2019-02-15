052e5d111713b64e0703684eb4e0d7b1e2053a3f
Yanni Kashoqa <YANNI_KASHOQA@msn.com>
2019-02-15 11:37:36 -0500

    Modify aliases to full commands

--- DSACTool.ps1
+++ DSACTool.ps1
@@ -15,56 +15,56 @@ $MenuList = @"
 	1: Search for a Rule by Hash Value.
 	2: List AC Global Ruleset to Screen.
 	3: Export AC Global Ruleset to a File.
-	
+
 	4: Add a New Block Rule.
 	5: Add New Rules by Answer File.
-	
+
 	6: Delete a Rule by ID.
 	7: Delete a Rule by Hash.
 	8: Delete Rules by Answer File.
-	
+
 	Q: Quit.
 "@
 
-Function MyLog { 
+Function MyLog {
     param (	[parameter(Mandatory=$true)] $OutputFile,
-			[parameter(Mandatory=$true)] $msg	) 
-			
+			[parameter(Mandatory=$true)] $msg	)
+
 		Write-Output "$msg" | Out-File $OutputFile -append
-} 
+}
 
 
 Function Connect-DSM {
 	[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 	[System.Net.ServicePointManager]::ServerCertificateValidationCallback={$true}
-		
+
 	$DSM_Cred	= Get-Credential -Message "Enter DSM Credentials"
 	$DSM_ID		= $DSM_Cred.GetNetworkCredential().UserName
 	$DSM_PASS	= $DSM_Cred.GetNetworkCredential().Password
-	
+
 	$creds = @{
 		dsCredentials = @{
 			userName = $DSM_ID
 	    	password = $DSM_PASS
 			}
 	}
-	$AuthData = $creds | ConvertTo-Json			
-	
+	$AuthData = $creds | ConvertTo-Json
+
 	try{
-		$Global:sID = Invoke-RestMethod -Uri $authUri -Method Post -Body $AuthData -Headers $headers 		
+		$Global:sID = Invoke-RestMethod -Uri $authUri -Method Post -Body $AuthData -Headers $headers
 	}
 	catch{
-		echo "[ERROR]	Failed to logon to DSM.	$_"
-		echo "An error occurred during authentication. Verify username and password and try again. `nError returned was: $($_.Exception.Message)"
-		Exit 
+		Write-Host "[ERROR]	Failed to logon to DSM.	$_"
+		Write-Host "An error occurred during authentication. Verify username and password and try again. `nError returned was: $($_.Exception.Message)"
+		Exit
 	}
-	
+
 	$cookie = new-object System.Net.Cookie
 	$cookie.name = "sID"
 	$cookie.value =  $sID
 	$cookie.domain = $manager
 	$Global:WebSession = new-object Microsoft.PowerShell.Commands.WebRequestSession
-	$WebSession.cookies.add($cookie)	
+	$WebSession.cookies.add($cookie)
 }
 
 function ListRules {
@@ -83,26 +83,26 @@ function ExportRules {
 	$RuleCount = $MyRules.rules.Count
 	MyLog -OutputFile $ReportFile -msg "RuleID	SHA256	Action	Description"
 	$i = 0
-	Do {		
+	Do {
 		$RuleID = $MyRules.rules[$i].ruleID
 		$SHA256 = $MyRules.rules[$i].sha256
 		$Action = $MyRules.rules[$i].action
 		$Description = $MyRules.rules[$i].description
 
 		$DRSData = "$RuleID	$SHA256	$Action	$Description"
-		#echo $DRSData
+		#Write-Host $DRSData
 		MyLog -OutputFile $ReportFile -msg $DRSData
 		$i++
 	} While ($i -lt $RuleCount)
 	Write-Host "Global Rulesets has been exported"
-	pause 
-	clear
+	pause
+	Clear-Host
 }
 
 function AddRule {
 	param (	[parameter(Mandatory=$true)] $SHA,
 			[parameter(Mandatory=$true)] $Description,
-			[parameter(Mandatory=$true)] $Action	) 
+			[parameter(Mandatory=$true)] $Action	)
 
 	$GlobalRule = @{
 		AddGlobalRulesetRulesRequest = @{
@@ -116,17 +116,17 @@ function AddRule {
 
 	$RuleID = LookupRuleID -Hash $SHA
 	if ($RuleID -ne $Null) {
-		write-Host "Rule with the following Hash already exist: $SHA"	
+		write-Host "Rule with the following Hash already exist: $SHA"
 	} Else {
-		write-Host "Adding new rule."	
+		write-Host "Adding new rule."
 		$GlobalRuleJSON = $GlobalRule | ConvertTo-Json
 		$URI = $managerUri + "rulesets/global/rules"
 		try{
-			$RulesObj = Invoke-RestMethod -Uri $URI -Method Post -WebSession $WebSession -Body $GlobalRuleJSON -Headers $headers		
+			$RulesObj = Invoke-RestMethod -Uri $URI -Method Post -WebSession $WebSession -Body $GlobalRuleJSON -Headers $headers
 		}
 		catch{
-			echo "[ERROR]	Failed to create rule.	$_"
-			echo "An error occurred during rule creation. Error returned was: $($_.Exception.Message)"
+			Write-Host "[ERROR]	Failed to create rule.	$_"
+			Write-Host "An error occurred during rule creation. Error returned was: $($_.Exception.Message)"
 		}
 	}
 }
@@ -138,7 +138,7 @@ function AddBlockRule {
 	AddRule -SHA $Hash -Description $Description -Action $Action
 	write-Host "Adding new rule Completed."
 	pause
-	clear
+	Clear-Host
 }
 
 Function AddRulesList {
@@ -154,15 +154,15 @@ Function AddRulesList {
 }
 
 function DelRule {
-	param (	[parameter(Mandatory=$true)] $RuleID	) 
+	param (	[parameter(Mandatory=$true)] $RuleID	)
 
 	$URI = $managerUri + "rulesets/global/rules/" + $RuleID
 	try{
-		$RulesObj = Invoke-RestMethod -Uri $URI -Method Delete -WebSession $WebSession -Headers $headers	
+		$RulesObj = Invoke-RestMethod -Uri $URI -Method Delete -WebSession $WebSession -Headers $headers
 	}
 	catch{
-		echo "[ERROR]	Failed to Delete rule.	$_"
-		echo "An error occurred during rule creation. Error returned was: $($_.Exception.Message)"
+		Write-Host "[ERROR]	Failed to Delete rule.	$_"
+		Write-Host "An error occurred during rule creation. Error returned was: $($_.Exception.Message)"
 	}
 }
 
@@ -171,51 +171,51 @@ function DelRuleByID {
 	DelRule -RuleID $RuleID
 	Write-Host "Rule ID $RuleID has been deleted"
 	Pause
-	Clear
+	Clear-Host
 }
 
 function DelRuleByHash {
 	$SHA256 = Read-Host "Please Enter The SHA256 Hash Value"
 	$RuleID = LookupRuleID -Hash $SHA256
-	
+
 	If ($RuleID -eq $Null) {
-		Write-Host "Hash does not exist"	
+		Write-Host "Hash does not exist"
 	} Else {
 		DelRule -RuleID $RuleID
 		Write-Host "Rule ID $RuleID has been deleted"
-	}	
+	}
 	Pause
-	Clear
+	Clear-Host
 }
 
 function DelRuleList {
 	$HashList = Get-Content $DelSourceFile
 	Foreach ($Hash in $HashList) {
-		$RuleID = LookupRuleID -Hash $Hash		
+		$RuleID = LookupRuleID -Hash $Hash
 		If ($RuleID -eq $Null) {
-			Write-Host "Hash does not exist"	
+			Write-Host "Hash does not exist"
 		} Else {
 			DelRule -RuleID $RuleID
 			Write-Host "Rule ID $RuleID has been deleted"
 		}
 	}
 	Pause
-	Clear
+	Clear-Host
 }
 
 function SearchRules {
 	$SHA256 = Read-Host "Please Enter The SHA256 Hash Value"
 	$RuleID = LookupRuleID -Hash $SHA256
-	
+
 	If ($RuleID -eq $Null) {
-		Write-Host "Hash does not exist"	
-	} Else {	
+		Write-Host "Hash does not exist"
+	} Else {
 		Write-Host "Your Rule ID is: $RuleID"
 	}
 }
 
 Function LookupRuleID {
-	param (	[parameter(Mandatory=$true)] $Hash ) 
+	param (	[parameter(Mandatory=$true)] $Hash )
 	$URI = $managerUri + "rulesets/global"
 	$RulesObj = Invoke-RestMethod -Uri $URI -Method Get -WebSession $WebSession
 	$Ruleset = $RulesObj.DescribeGlobalRulesetResponse.ruleset
@@ -232,7 +232,7 @@ Function LookupRuleID {
 	return $null
 }
 
-Clear
+Clear-Host
 Connect-DSM
 
 do {
